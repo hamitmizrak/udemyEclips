c0035e6a27e4fb139bd0b9d093db0aeefb2e3db0
Hamit Mizrak <hamitmizrak@gmail.com>
2020-12-18 00:41:28 +0300

    Commit -3

---
+++ src/main/java/com/sirketadi/proje/Deneme4.java
@@ -0,0 +1,9 @@
+package com.sirketadi.proje;
+
+public class Deneme4 {
+
+	public static void main(String[] args) {
+		System.out.println("Merhabalar Git/GitHub Eclipse öğrenen güzel insanlar");
+	}
+
+}
---
+++ src/main/java/com/sirketadi/proje/Deneme5.java
@@ -0,0 +1,9 @@
+package com.sirketadi.proje;
+
+public class Deneme5 {
+
+	public static void main(String[] args) {
+		System.out.println("Merhabalar Git/GitHub Eclipse öğrenen güzel insanlar");
+	}
+
+}
