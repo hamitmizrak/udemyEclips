b822692801628815b9438b05616130b8e87b0a4e
Hamit Mizrak <hamitmizrak@gmail.com>
2020-12-18 00:22:08 +0300

    İlk commitim

---
+++ src/main/java/com/sirketadi/proje/Deneme.java
@@ -0,0 +1,9 @@
+package com.sirketadi.proje;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Merhabalar Git/GitHub Eclipse öğrenen güzel insanlar");
+	}
+
+}
