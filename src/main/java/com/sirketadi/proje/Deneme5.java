d051d0d08449673661872fc2903f4d88a60b6bad
Hamit Mizrak <hamitmizrak@gmail.com>
2020-12-18 01:05:38 +0300

    Commit -3 new Branch

--- src/main/java/com/sirketadi/proje/Deneme5.java
+++ src/main/java/com/sirketadi/proje/Deneme5.java
@@ -5,5 +5,5 @@ public class Deneme5 {
 	public static void main(String[] args) {
 		System.out.println("Merhabalar Git/GitHub Eclipse öğrenen güzel insanlar");
 	}
-
+//Yeni branchten gelene alan
 }
